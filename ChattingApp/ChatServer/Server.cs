fa043125be4743bab9fb9863db42b4a76a19cca5
Jakub Formanek <xforman2@fi.muni.cz>
2023-06-20 17:10:59 +0200

    added accepting messages to server

--- ChattingApp/ChatServer/Server.cs
+++ ChattingApp/ChatServer/Server.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatServer
 {
-    public static class Server
+    public class Server
     {
 
         public static int Port { get; set; } = 5555;
@@ -18,15 +20,29 @@ namespace ChatServer
         {
            
             var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-  
             IPAddress[] addresses = Dns.GetHostEntry(IPAddress.Loopback).AddressList;
             IPAddress hostIP = addresses[0];
             IPEndPoint ep = new IPEndPoint(hostIP, Port);
             socket.Bind(ep);
             socket.Listen(100);
+            while (true)
+            {
+                byte[] buffer = new byte[256];
+                var connectionSocket = socket.Accept();
+                int bytes = connectionSocket.Receive(buffer);
+                if (bytes > 256)
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                }
+                
 
+            }
+
+            
 
         }
+
+  
 
 
 
